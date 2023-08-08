931e236d1f0e4b1a23ad96f9cdec5541269805d3
Anton Åsbrink <anton@axenu.com>
2023-08-08 08:37:53 +0200

    backend linting config from enkla kassan

---
+++ eslintrc-backend.js
@@ -0,0 +1,31 @@
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    plugins: [
+      "@typescript-eslint",
+      "prettier",
+      "unused-imports",
+      "jest"
+    ],
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/eslint-recommended",
+      "plugin:@typescript-eslint/recommended",
+      "prettier"
+    ],
+    rules: {
+      "no-console": 0,
+      "prettier/prettier": 2,
+      "unused-imports/no-unused-imports": "error",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/no-floating-promises": "error",
+      "no-fallthrough":"off"
+    },
+    parserOptions: {
+      "project": "./tsconfig.json",
+      "sourceType": "module"
+    },
+    env: {
+      "jest": true
+    },
+}
