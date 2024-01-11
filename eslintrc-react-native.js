f4d04de97ed6cffeebb896cc46b8eca863490399
Axenu <simon@axenu.com>
2024-01-11 11:37:24 +0100

    remove import react requirement

--- eslintrc-react-native.js
+++ eslintrc-react-native.js
@@ -57,7 +57,8 @@ module.exports = {
     "jest/no-focused-tests": "error",
     "jest/no-identical-title": "error",
     "jest/prefer-to-have-length": "warn",
-    "jest/valid-expect": "error"
+    "jest/valid-expect": "error",
+    "react-native/no-unused-styles": "error",
   },
   extends: [
     'plugin:@typescript-eslint/recommended',
